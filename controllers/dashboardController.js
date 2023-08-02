c903b3ee5f02dd5444e97cf4fa8609aef371bf15
alirezanava72 <alirezanava72@gmail.com>
2023-08-02 11:59:50 +1000

    R&U working

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -4,6 +4,7 @@ const Note = require('../models/notes');
 const mongoose = require('mongoose');
 
 
+// GET / Dashboard
 
 exports.dashboard = async (req, res) => {
     const locals = {
@@ -11,8 +12,53 @@ exports.dashboard = async (req, res) => {
         description: 'Free NodeJs Notes App.',
     }
 
-    res.render('dashboard/index', {
-        locals,
+    try {
+        const notes = await Note.find({});
+        
+        res.render('dashboard/index', {
+          userName: req.user.firstName,
+          locals,
+          notes,
+          layout: '../views/layouts/dashboard'
+    });
+    } catch (error) {
+        
+    }
+
+}
+
+
+// GET view specific note
+
+exports.dashboardViewNote = async(req, res) => {
+    const note = await Note.findById({ _id: req.params.id }).where({ user: req.user.id }).lean();
+
+if (note) {
+    res.render('dashboard/view-note', {
+        noteID: req.params.id,
+        note,
         layout: '../views/layouts/dashboard'
     });
+} else {
+    res.send("Something went wrong.")
+}
+
+
+}
+
+
+// PUT update specfic note
+
+exports.dashboardUpdateNote = async(req, res) => {
+ try {
+    await Note.findOneAndUpdate(
+        {_id: req.params.id},
+        { title: req.body.title, body : req.body.body }
+        ).where( {user: req.user.id });
+
+        res.redirect('/dashboard');
+
+ } catch (error) {
+    console.log(error);
+ }
 }
